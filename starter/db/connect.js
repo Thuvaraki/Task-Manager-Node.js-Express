59ee82912c4e8283452c971d84c8f2fa4e8db276
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-01-28 14:33:31 +0530

    updated

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -6,3 +6,6 @@ const connectDB = (url) => {
 };
 
 module.exports = connectDB;
+
+// while clone the project from github have to setup  .env
+// MONGO_URI =   mongodb+srv://Thuva:7638@nodeexpressproject.rlw9nxr.mongodb.net/Task-Manager?retryWrites=true&w=majority
